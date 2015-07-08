0ca93b1e57e8dc6c3bfb731442d83ca84b796ae7
Joe Cochran <j_cochran@me.com>
2015-07-07 22:27:03 -0700

    setting up project with npm and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,28 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 
 gulp.task('sass', function(){
     gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
+
+gulp.task('scripts', function(){
+    // create modernizr.js from node module
+    gulp.src('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js')
+        .pipe(gulp.dest('./js/'));
+    // gulp.src('node_modules/foundation/vendor/jquery.js')
+});
+
+gulp.task('connect', function() {
+  connect.server();
+});
+
 gulp.task('watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
-gulp.task('default', ['sass', 'watch']);
+
+
+gulp.task('default', ['sass', 'scripts', 'connect', 'watch']);
