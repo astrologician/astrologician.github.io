e8bdb18c5a5e9225f866e9f13e2e6e019b1b6fdd
Joe Cochran <j_cochran@me.com>
2015-08-14 08:53:59 -0700

    some optimization for speed

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function(){
     gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
         .pipe(gulp.dest('./css'));
 });
 
